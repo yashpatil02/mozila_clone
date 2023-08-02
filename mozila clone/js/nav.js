f6789781f36f7975e9078ef8335855ba0a8fb4af
yashpatil02 <98704483+yashpatil02@users.noreply.github.com>
2023-08-02 11:40:33 +0530

    Add files via upload

---
+++ mozila clone/js/nav.js
@@ -0,0 +1,20 @@
+$(function(){
+    var scroll  =$(document).scrollTop();
+    var navHeight = $('.nav-area').outerHeight();
+  
+    $(window).scroll(function(){
+      var scrolled = $(document).scrollTop();
+      if(scrolled >navHeight){
+        $('.nav-area').addClass('animate');
+      }else{
+        $('.nav-area').removeClass('animate');
+      }
+      if(scrolled > scroll){
+        $('.nav-area').removeClass('sticky');
+      }else{
+        $('.nav-area').addClass('sticky');
+      }
+      scroll = $(document).scrollTop();
+    })
+  })
+  
